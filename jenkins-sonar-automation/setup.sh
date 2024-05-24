a008b96db08939a3b1622f73ce8ece11545fdbf8
Mahesh Pansare <138249606+epic-croswords@users.noreply.github.com>
2024-05-24 22:58:53 +0530

    Update setup.sh

--- jenkins-sonar-automation/setup.sh
+++ jenkins-sonar-automation/setup.sh
@@ -23,11 +23,11 @@ sudo systemctl status docker
 # install jenkins
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y
+sudo apt-get update
+sudo apt-get install jenkins
 
 #service enable-start jenkins 
 sudo systemctl enable jenkins
