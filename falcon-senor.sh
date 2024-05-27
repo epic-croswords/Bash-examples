9222d0fbc8676346f93fd9ba761ec2c6b55a27cd
Mahesh Pansare <138249606+epic-croswords@users.noreply.github.com>
2024-05-27 11:33:19 +0530

    Create falcon-senor.sh

---
+++ falcon-senor.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -x 
+
+cd /tmp
+os-release=awk -F= '/^ID=/ {gsub(/"/, "", $2); print $2}' /etc/os-release
+if [ "$os-release" == "redhat"]; then
+wget https://isrcportal.magna.global/repo/CrowdStrike/Linux/latest/falcon-sensor-7.11.0-16405.el7.x86_64.rpm --no-check-certificate
+
+chmod 777 falcon-sensor-7.11.0-16405.el7.x86_64.rpm
+yum localinstall falcon-sensor-7.11.0-16405.el7.x86_64.rpm -y
+sudo /opt/CrowdStrike/falconctl -s --cid=
+
+systemctl enable falcon-sensor
+systemctl start falcon-sensor
+systemctl status falcon-sensor
+else 
+
+cd /tmp
+wget https://isrcportal.magna.global/repo/CrowdStrike/Linux/latest/falcon-sensor_7.11.0-16405_amd64.deb --no-check-certificate
+chmod 777 falcon-sensor_7.11.0-16405_amd64.deb
+dpkg -i falcon-sensor_7.11.0-16405_amd64.deb
+sudo /opt/CrowdStrike/falconctl -s --cid=
+
+systemctl enable falcon-sensor
+systemctl start falcon-sensor
+systemctl status falcon-sensor
+fi
