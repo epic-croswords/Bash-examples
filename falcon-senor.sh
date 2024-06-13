2bf138a798c7a2d623224caecb603cd0db811376
Mahesh Pansare <138249606+epic-croswords@users.noreply.github.com>
2024-06-13 13:25:33 +0530

    added inline $1 to accept the input

--- falcon-senor.sh
+++ falcon-senor.sh
@@ -4,7 +4,7 @@ set -x
 #variables
 URL_rpm=https://isrcportal.magna.global/repo/CrowdStrike/Linux/latest/falcon-sensor-7.11.0-16405.el7.x86_64.rpm
 URL_deb=https://isrcportal.magna.global/repo/CrowdStrike/Linux/latest/falcon-sensor_7.11.0-16405_amd64.deb
-CID=
+CID=$1
 
 cd /tmp
 os-release=awk -F= '/^ID=/ {gsub(/"/, "", $2); print $2}' /etc/os-release
